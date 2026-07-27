0227dbc4efca5c35fec610eca67e26dfabd3c350
rajborge <rajborge3085@gmail.com>
2026-07-27 22:50:53 +0530

    Time: 43 ms (26.84%), Space: 46.4 MB (65.07%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] findNGE(int[] nums){
         int n=nums.length;
         int[] ans=new int[n];
-        Stack<Integer> st=new Stack<>();
+        Deque<Integer> st=new ArrayDeque<>();
 
         for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && nums[st.peek()]<nums[i]){
@@ -16,7 +16,7 @@ class Solution {
     public int[] findNSE(int[] nums){
         int n=nums.length;
         int[] ans=new int[n];
-        Stack<Integer> st=new Stack<>();
+        Deque<Integer> st=new ArrayDeque<>();
 
         for(int i=n-1;i>=0;i--){
             while(!st.isEmpty() && nums[st.peek()]>nums[i]){
@@ -30,7 +30,7 @@ class Solution {
     public int[] findPGEE(int[] nums){
         int n=nums.length;
         int[] ans=new int[n];
-        Stack<Integer> st=new Stack<>();
+        Deque<Integer> st=new ArrayDeque<>();
 
         for(int i=0;i<n;i++){
             while(!st.isEmpty() && nums[st.peek()]<=nums[i]){
@@ -44,7 +44,7 @@ class Solution {
     public int[] findPSEE(int[] nums){
         int n=nums.length;
         int[] ans=new int[n];
-        Stack<Integer> st=new Stack<>();
+        Deque<Integer> st=new ArrayDeque<>();
 
         for(int i=0;i<n;i++){
             while(!st.isEmpty() && nums[st.peek()]>=nums[i]){
