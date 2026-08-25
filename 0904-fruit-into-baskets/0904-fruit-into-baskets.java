415f4d6328c2fabe3f3b35f7b321e4b9e72b3003
rajborge <rajborge3085@gmail.com>
2026-08-25 22:58:24 +0530

    Time: 54 ms (66.75%), Space: 70.6 MB (29.13%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int left=0;
+        int right=0;
+        int maxLength=0;
+
+        Map<Integer,Integer> map=new HashMap<>();
+
+        while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>=3){
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left])==0) map.remove(fruits[left]);
+                left++;
+            }
+            maxLength=Math.max(maxLength,right-left+1);
+            right++;
+        }
+        return maxLength;
+    }
+}
