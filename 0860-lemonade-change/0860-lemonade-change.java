13ae32a56e397a4a01f3692d0157b607fc110bf4
rajborge <rajborge3085@gmail.com>
2026-09-06 21:44:37 +0530

    Time: 2 ms (97.81%), Space: 72.6 MB (30.54%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        if (bills[0]!=5) return false;
+        int five=0,ten=0;
+        for(int bill:bills){
+            if(bill==5) five++;
+            else if(bill==10){
+                if(five>0){
+                five--;
+                ten++;
+                }
+                else return false;
+            }
+            else{
+                if(ten>0 && five>=1){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
