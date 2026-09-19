42ed59fc5c51664a07aa02a1d88cf3cbf13df41d
rajborge <rajborge3085@gmail.com>
2026-09-19 22:35:19 +0530

    Time: 1 ms (99.19%), Space: 46.5 MB (35.17%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder==null || postorder==null || inorder.length!=postorder.length) return null;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return buildTree(inorder,0,inorder.length-1,postorder,0,postorder.length-1,map);
+    }
+
+    public TreeNode buildTree(int[] inorder,int inStart,int inEnd,int[] postorder,int postStart,int postEnd,HashMap<Integer,Integer> map){
+        if(inStart>inEnd || postStart>postEnd) return null;
+
+        TreeNode root=new TreeNode(postorder[postEnd]);
+
+        int inRoot=map.get(postorder[postEnd]);
+        int numLeft=inRoot-inStart;
+        root.left=buildTree(inorder,inStart,inRoot-1,postorder,postStart,postStart+numLeft-1,map);
+        root.right=buildTree(inorder,inRoot+1,inEnd,postorder,postStart+numLeft,postEnd-1,map);
+        return root;
+    }
+}
